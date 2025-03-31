273f88f6bf48a71e37eace48eb766bf4ad0b08db
Vladimir Kobal <vlvkobal@gmail.com>
2025-03-31 16:01:07 +0200

    Print hex codes for special characters in lang (#208)

--- wireshark/source/packet-ja4.c
+++ wireshark/source/packet-ja4.c
@@ -415,19 +415,40 @@ conn_info_t *conn_lookup (char proto, int stream) {
 }
 
 void decode_http_lang(wmem_strbuf_t **out, const char *val) {
-	char lang[5] = "0000"; 
-	int len = 0;
+	wmem_strbuf_t *lang = wmem_strbuf_new(wmem_packet_scope(), "");
+	int count = 0;
 
-	for (int i=0; i<5; i++) {
-		if ((val[i] == ',') || (val[i] == ';') || (len == 4) || (val[i] == '\0')) {
+	// Format the language string
+	for (int i = 0; i < 5; i++) {
+		if ((val[i] == ',') || (val[i] == ';') || (count >= 4) || (val[i] == '\0')) {
 			break;
 		}
-		if ((!g_ascii_isspace(val[i])) && (val[i] != '-')) {
-			lang[len++] = g_ascii_tolower(val[i]);
+		if ((g_ascii_isspace(val[i])) || (val[i] == '-')) {
+			continue;
+		}
+		if (g_ascii_isalpha(val[i]) || g_ascii_isdigit(val[i])) {
+			wmem_strbuf_append_c(lang, g_ascii_tolower(val[i]));
+			count++;
+		}
+		else {
+			// Convert a special character to hex
+			static const char hex[16] = { '0', '1', '2', '3', '4', '5', '6', '7',
+										'8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+			wmem_strbuf_append_c(lang, hex[(val[i] >> 4) & 0xF]);
+			wmem_strbuf_append_c(lang, hex[(val[i] >> 0) & 0xF]);
+			count += 2;
 		}
 	}
 
-	wmem_strbuf_append_printf(*out, "%s", lang);
+	// Ensure we have 4 characters
+	if (count < 4) {
+		wmem_strbuf_append_c_count(lang, '0', 4 - count);
+	}
+	wmem_strbuf_truncate(lang, 4);
+
+	wmem_strbuf_append_printf(*out, "%s", wmem_strbuf_get_str(lang));
+
+	wmem_strbuf_destroy(lang);
 }
 
 void decode_http_version(wmem_strbuf_t **out, const char *val) {
